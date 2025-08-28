d50884876224e896a312008651f2139f5845d1fd
jscontreras <sebastian.contreras@vercel.com>
2025-08-28 15:29:44 -0500

    Adding green to vercel banner.

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,41 @@
-import { type NextRequest, NextResponse } from "next/server"
+import { type NextRequest, NextResponse } from "next/server";
+import { get } from "@vercel/edge-config";
+
+type ABReleases = {
+  threshold: number;
+  active: boolean;
+};
 
 export async function middleware(req: NextRequest) {
-  // Set for the legacy site
-  if (process.env.ENABLE_PROXY === 'disabled') {
+  const abReleases = await get<ABReleases>("abReleases");
+  console.log('abReleases', abReleases);
+
+  if (!abReleases?.active) {
+    console.log('abReleases is not active');
     return NextResponse.next();
   }
 
-  const url = new URL(req.url);
-  url.hostname = 'v0pocs.tc-vercel.dev';
-  url.port = ''; // Ensure the port is ignored
-  url.protocol = 'https';
-  return await NextResponse.rewrite(url.toString());
+  let gbChoice = req.cookies.get("gb_choice")?.value;
+  const response = NextResponse.next();
+
+  if (!gbChoice) {
+    const randomNumber = Math.random();
+    gbChoice = randomNumber < abReleases.threshold ? "true" : "false";
+    response.cookies.set("gb_choice", gbChoice);
+  }
+
+  if (gbChoice === "true") {
+    const url = new URL(req.url);
+    const proxiedSiteUrl = process.env.PROXIED_SITE_URL;
+
+    if (proxiedSiteUrl) {
+      const proxiedUrl = new URL(proxiedSiteUrl);
+      url.hostname = proxiedUrl.hostname;
+      url.port = proxiedUrl.port;
+      url.protocol = proxiedUrl.protocol;
+      return NextResponse.rewrite(url, response);
+    }
+  }
+
+  return response;
 }
